f9b09430caa1c837e90fb16c544fda4ff3200722
PrakritiPandey2024 <202401164@daiict.ac.in>
2025-04-03 14:30:54 +0530

    first-unique-character-in-a-string.cpp

--- first-unique-character-in-a-string.cpp
+++ first-unique-character-in-a-string.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        vector<int> freq(26, 0); // For 'a' to 'z'
-
-        // Step 1: Count frequency of each character
+        vector<int> freq(26, 0); 
         for (char c : s) {
             freq[c - 'a']++;
         }
 
-        // Step 2: Find the first unique character
         for (int i = 0; i < s.length(); ++i) {
             if (freq[s[i] - 'a'] == 1) {
                 return i;
             }
         }
 
-        return -1; // No unique character
+        return -1; 
     }
 };
